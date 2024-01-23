3e9132639999cdfecae3c3066bc7375711a2da40
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-01-23 16:14:28 +0600

    second

--- play.js
+++ play.js
@@ -64,7 +64,7 @@
 // const toArray = (arg1, arg2, arg3) =>{
 //     return [arg1,arg2,arg3];   // beshi argument pathale big data onk hard hoy
 // }
-const toArray = (...arg) =>{   ///rest operator atto line likha lage na
-    return arg;
-}
-console.log(toArray(1,2,3,4,5));
+// const toArray = (...arg) =>{   ///rest operator atto line likha lage na
+//     return arg;
+// }
+// console.log(toArray(1,2,3,4,5));
