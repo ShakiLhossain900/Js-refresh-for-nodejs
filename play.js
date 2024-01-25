078d0f0f47cb2fe36af0f992a994053e2ffcabf5
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2024-01-25 09:35:27 +0600

    shakil

--- play.js
+++ play.js
@@ -1,42 +1,42 @@
-var  name = "shakil"; // for the use of const we don't have plan to change the const keyword
-let age = 26;  // amara let use korle amara change korte parbu
-const hasHobbies = true;
- 
-//const name='moure'; //ata kaj korbe na karon akhane const use koreci 
-var name = "moure"; // for the use of also can run as like let 
-let =30;
-
-function summarize(name, age, hasHobbies) {
-  return ( "name is " + name + " age is " + age + " and i had a " + hasHobbies);
-}
-console.log(summarize(name, age, hasHobbies));
-
-//ARROW FUNCTION 
-// var  name = "shakil";
-// let age = 26; 
+// var  name = "shakil"; // for the use of const we don't have plan to change the const keyword
+// let age = 26;  // amara let use korle amara change korte parbu
 // const hasHobbies = true;
  
+// //const name='moure'; //ata kaj korbe na karon akhane const use koreci 
+// var name = "moure"; // for the use of also can run as like let 
+// let =30;
 
-const  summarize=(name, age, hasHobbies)=> {
-  return ( "name is " + name + " age is " + age + " and i had a " + hasHobbies);
-}
-console.log(summarize(name, age, hasHobbies));
+// function summarize(name, age, hasHobbies) {
+//   return ( "name is " + name + " age is " + age + " and i had a " + hasHobbies);
+// }
+// console.log(summarize(name, age, hasHobbies));
+
+// //ARROW FUNCTION 
+// // var  name = "shakil";
+// // let age = 26; 
+// // const hasHobbies = true;
+ 
+
+// const  summarize=(name, age, hasHobbies)=> {
+//   return ( "name is " + name + " age is " + age + " and i had a " + hasHobbies);
+// }
+// console.log(summarize(name, age, hasHobbies));
 
 
-// // const addOne=(a,b)=> a+b;
-// const addRandom=()=>1+1;
-// console.log(addRandom())
+// // // const addOne=(a,b)=> a+b;
+// // const addRandom=()=>1+1;
+// // console.log(addRandom())
 
-object 
-const person ={
-    name: 'shakil',
-    age:26,
-    // great:()=>{ //arrow funtion kaj korbe na old function use korte hobe 
-    great: function(){
-        console.log(' hi this is shakil '+ this.name +' name age is '+ this.age);
-    }
-};
-person.great();
+// object 
+// const person ={
+//     name: 'shakil',
+//     age:26,
+//     // great:()=>{ //arrow funtion kaj korbe na old function use korte hobe 
+//     great: function(){
+//         console.log(' hi this is shakil '+ this.name +' name age is '+ this.age);
+//     }
+// };
+// person.great();
 
 //ARRYAY and ARRAY METHOD
 // const hobies =['Shports ', 'cooking'];
@@ -92,4 +92,13 @@ person.great();
 // const hasHobbies =['dev developer', 'and researcher'];
 // const [hobby1,hobby2] = hasHobbies;
 // console.log(hobby1,hobby2);
-hi this is shakil
+
+// Destructuring 
+const person = {
+  name:'Max',
+  age : 29,
+  greet(){
+    console.log('Hi! i am ' + this.name);
+  }
+}
+person.greet();
